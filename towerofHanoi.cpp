bc5ffd38baa2245d88e5cb2d27a34e8471a135c3
tigerpa616 <masarskydaniel@gmail.com>
2021-03-24 14:01:38 -0400

    Update towerofHanoi.cpp

--- towerofHanoi.cpp
+++ towerofHanoi.cpp
@@ -13,3 +13,21 @@ struct State {
 };
 
 std::vector<State> border;
+
+//  h (x)= (the total number of disks of left and middle poles) + 2*（number of disks that in the right pole and smaller than any disk that in left or middle poles）
+int hValue(std::string p1, std::string p2, std::string p3)
+{
+    std::vector<char> findings;
+    int h = 0;
+    if(p1 != "0")
+    {
+        for(int i = 0; i < p1.length(); i++)
+        {
+            if(p1[i] == 'T' || p1[i] == 'M' || p1[i] == 'B')
+            {
+                findings.push_back(p1[i]);
+                h = h + 1;
+            }
+        }
+    }
+}
